a041446b433c17d5f275ffdc4fbd4c61179f03c8
Manuela <manuelamunozlujan0@gmail.com>
2025-03-15 09:41:14 -0500

    us1/create register

---
+++ nomina.py
@@ -0,0 +1,25 @@
+#US1- Create register
+users = []
+
+def createUser():
+    user = []
+    id = int(input("Ingrese su documento de identidad"))
+    user.append(id)
+    user_name = input("Ingrese su nombre")
+    user.append(user_name)
+    user_last_name = input("Ingrese su apellido")
+    user.append(user_last_name) 
+    phone = input("Ingrese su teléfono")
+    user.append(phone)
+    user_email = input("Ingrese su correo electrónico")
+    user.append(user_email)
+    user_password = input("Ingrese su correo contraseña")
+    user.append(user_password)
+    users.append(user)
+    
+createUser()
+createUser()
+
+print(users)
+
+    
